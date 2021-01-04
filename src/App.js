25bf53bb57a90636eaddc275f6aa13090df9b5f3
shreyas ks <shreyasks23@live.com>
2021-01-04 15:47:19 +0530

    improvements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import User from './Components/Users/Users';
+import NavBar from './Components/UI/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    userName: 'shreyas',   
+  }
+  
+
+  render() {
+    return (
+      <div className="App container fluid">
+        <NavBar/>
+        <User/>
+      </div>
+    );
+  }
 }
 
 export default App;
